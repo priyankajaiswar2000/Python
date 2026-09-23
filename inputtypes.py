3e0a46f167d7d3a2391cc59154dff0e0e7928d79
priyankajaiswar2000 <priyankajaiswar6387@gmail.com>
2026-09-23 16:39:46 +0530

    Add datatype

---
+++ inputtypes.py
@@ -0,0 +1,14 @@
+Name = input("Enter your name :")
+print(Name)
+
+Age = int(input("Enter your age :"))
+print(Age)
+
+Course = input("Enter your course :")
+print(Course)
+
+Address = input("Enter your address :")
+print(Address)
+
+Number = int(input("Enter your number :"))
+print(Number)
